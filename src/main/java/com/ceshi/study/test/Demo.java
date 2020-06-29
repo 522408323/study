14aa538888095670eee0b3548a3c6705434f3338
shenyafei1 <shenyafei@labifenqi.com>
2020-06-29 10:11:35 +0800

    添加java8流的demo

--- src/main/java/com/ceshi/study/test/Demo.java
+++ src/main/java/com/ceshi/study/test/Demo.java
@@ -1,5 +1,16 @@
 package com.ceshi.study.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @ClassName: Demo
  * @Author: shenyafei
@@ -12,5 +23,31 @@ public class Demo {
         StringBuffer s2 = new StringBuffer("abc");
         System.out.println("=="+s1.equals(s2));
         System.out.println(new String("123456".toCharArray(),1,3));
+        String data = "{\"author_state\":0}";
+        JSONObject json = JSON.parseObject(data);
+        int authorState = json.getIntValue("author_state");
+        int failNum = json.getIntValue("fail_num");
+        System.out.println("====authorState:"+authorState + ",,failNum:" + failNum);
+
+        Map<String,String> map11 = new HashMap<>();
+        map11.put("t","123");
+
+        Map<String,String> map = new ConcurrentHashMap<>();
+        map.put("a","1111");
+
+        String str = "{\"payMethods\":[{\"code\":\"RTC001\",\"icon\":\"\",\"isApply\":\"N\",\"name\":\"银行卡支付\"},{\"code\":\"RTC002\",\"icon\":\"\",\"isApply\":\"N\",\"name\":\"验密支付\"}, {\"code\":\"RTC003\",\"icon\":\"\",\"isApply\":\"Y\",\"name\":\"一键  付\"}],\"payReqDto\":{\"busAppId\":\"962813278\",\"busCode\":\"BL001\",\"cName\":\"生活口语\",\"credCode\":\"CC001\",\"frontJumpUrl\":\"https://txuedai.labifenqi.com/jyfq-front-custom/order/orderList\",\"orderDetailId\":\"HhqxA3l0Vt4ynXbHNsS\",\"orderId\":\"HhqxA3l0Vt4ynXbHNsS\",\"pid\":\"1471964\",\"planRepayDate\":1579017600000,\"platCode\":\"PL001\",\"receiptType\":\"G001\",\"repayAmt\":44.52,\"reqSno\":\"2eq0Nr9fY1Ko4H6oRmC\"} }";
+        System.out.println("-=========="+JSONObject.toJSONString(JSONObject.parseObject(str)));
+        String bankNo = "12345678";
+        System.out.println(bankNo = bankNo.substring(bankNo.length()-4, bankNo.length()));
+        System.out.println("remainSeconds===="+getRemainSecondsOneDay());
     }
+
+    public static Long getRemainSecondsOneDay() {
+        Date currentDate = new Date();
+        LocalDateTime midnight = LocalDateTime.ofInstant(currentDate.toInstant(), ZoneId.systemDefault()).plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime currentDateTime = LocalDateTime.ofInstant(currentDate.toInstant(),ZoneId.systemDefault());
+        long seconds = ChronoUnit.SECONDS.between(currentDateTime, midnight);
+        return (Long) seconds;
+    }
+
 }
---
+++ src/main/java/com/ceshi/study/test/LambdaTest.java
@@ -0,0 +1,119 @@
+package com.ceshi.study.test;
+
+import com.ceshi.study.model.User;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName: LambdaTest
+ * @Author: shenyafei
+ * @Date: 2020/6/28
+ * @Desc
+ **/
+public class LambdaTest {
+
+   public static void filter(){
+       //过滤
+       List<String> list = new ArrayList<>();
+       list.add("a");list.add("b");list.add("c");
+       System.out.println("过滤前：");
+       list.stream().forEach(e-> System.out.println(e));
+       list = list.stream().filter(e -> e.equals("b")).collect(Collectors.toList());
+       System.out.println("过滤后：");
+       list.stream().forEach(e-> System.out.println(e));
+   }
+
+   public static void distinct(){
+       //去重
+       List<String> list = new ArrayList<>();
+       list.add("a");list.add("a");list.add("b");list.add("c");list.add("c");
+       System.out.println("去重前：");
+       list.stream().forEach(e-> System.out.println(e));
+       list = list.stream().distinct().collect(Collectors.toList());
+       System.out.println("去重后：");
+       list.stream().forEach(e-> System.out.println(e));
+   }
+
+   public static void distinctObject(){
+       List<User> list = new ArrayList<>();
+       User user = new User();
+       user.setId(1);
+       user.setName("张三");
+       user.setAge(20);
+       list.add(user);
+       list.add(user);
+       System.out.println("实体去重前：");
+       list.stream().forEach(e-> System.out.println(e.toString()));
+       list = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(User::getId))),ArrayList::new));
+       System.out.println("实体去重后：");
+       list.stream().forEach(e-> System.out.println(e.toString()));
+   }
+
+   public static void mapObject(){
+       //映射
+       List<User> list = new ArrayList<>();
+       User user = new User();
+       user.setId(1);
+       user.setName("张三");
+       user.setAge(20);
+       list.add(user);
+       User user2 = new User();
+       user2.setId(2);
+       user2.setName("张三2");
+       user2.setAge(22);
+       list.add(user2);
+       System.out.println("映射前：");
+       list.stream().forEach(e-> System.out.println(e.toString()));
+       List<Integer> finalList = list.stream().map(User::getId).collect(Collectors.toList());
+       System.out.println("映射后：");
+       finalList.stream().forEach(e-> System.out.println(e));
+   }
+
+   public static void sort(){
+       List<Integer> list = new ArrayList<>();
+       list.add(10); list.add(1);list.add(2);
+       System.out.println("ASCII排序前：");
+       list.stream().forEach(e-> System.out.println(e));
+       list = list.stream().sorted().collect(Collectors.toList());
+       System.out.println("ASCII排序后：");
+       list.stream().forEach(e-> System.out.println(e));
+       list = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+       System.out.println("ASCII倒叙排序后：");
+       list.stream().forEach(e-> System.out.println(e));
+   }
+
+    public static void moreSort(){
+        List<User> list = new ArrayList<>();
+        list.add(new User(1,"张三1",10));
+        list.add(new User(2,"张三2",20));
+        list.add(new User(3,"张三3",30));
+        System.out.println("复杂排序前：");
+        list.stream().forEach(e-> System.out.println(e.toString()));
+        list = list.stream().sorted(Comparator.comparing(User::getAge)).collect(Collectors.toList());
+        System.out.println("复杂排序后：");
+        list.stream().forEach(e-> System.out.println(e.toString()));
+        list = list.stream().sorted(Comparator.comparing(User::getAge).reversed()).collect(Collectors.toList());
+        System.out.println("复杂倒叙排序后：");
+        list.stream().forEach(e-> System.out.println(e.toString()));
+    }
+
+    public static void main(String[] args) {
+        //过滤
+        filter();
+        //去重
+        distinct();
+        distinctObject();
+        //映射
+        mapObject();
+        //匹配
+
+        //排序
+        sort();
+        //复杂排序
+        moreSort();
+    }
+}
