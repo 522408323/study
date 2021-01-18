f0de54297f3ee335e8a714cb6593555cdbaba6a6
shenyafei1 <shenyafei@labifenqi.com>
2021-01-18 15:27:47 +0800

    添加测试例子2

---
+++ src/main/java/com/ceshi/study/QuartzTask/CeshiTask.java
@@ -0,0 +1,14 @@
+package com.ceshi.study.QuartzTask;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: CeshiTask
+ * @Author: shenyafei
+ * @Date: 2021/1/15
+ * @Desc
+ **/
+@Component
+public class CeshiTask {
+
+}
--- src/main/java/com/ceshi/study/QuartzTask/TimeTask.java
+++ src/main/java/com/ceshi/study/QuartzTask/TimeTask.java
@@ -1,5 +1,7 @@
 package com.ceshi.study.QuartzTask;
 
+import com.ceshi.study.util.SpringContextUtil;
+
 /**
  * @ClassName: TimeTask
  * @Author: shenyafei
@@ -8,5 +10,17 @@ package com.ceshi.study.QuartzTask;
  **/
 public class TimeTask {
 
-
+    public static void main(String[] args) {
+       /* String a = "com.jdbb.bus.quartz.ser.job.task.CustomerTask";
+        a = "A";
+        String b = a.substring(a.lastIndexOf(".")+1,a.length());
+        b = b.substring(0,1).toLowerCase() + b.substring(1,b.length());
+        System.out.println(b);
+        Object bean = SpringContextUtil.getBean("ceshiTask");*/
+       CeshiTask c1 = new CeshiTask();
+        CeshiTask c2 = new CeshiTask();
+        CeshiTask  c3 = c2;
+        System.out.println(c1 == c2);
+        System.out.println(c2 == c3);
+    }
 }
